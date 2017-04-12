9c222a59eb5f2f3c3fa18204dae494de4d9fdcfe
Simon Guest <me@simonguest.com>
2017-04-12 10:21:12 -0700

    Basic dataset functionality

--- test/index.js
+++ test/index.js
@@ -1,4 +1,6 @@
 const istanbul = require('istanbul-middleware');
+const pg = require('pg');
+let pool = new pg.Pool();
 
 const url = require('url');
 const express = require('express');
@@ -11,12 +13,25 @@ router.use('/kill', () => {
 
 router.use('/coverage', istanbul.createHandler({verbose: true, resetOnGet: true}));
 
+router.use('/data', (req, res) => {
+  // check if path exists
+  if (req.path === '/default') {
+    pool.query(require('fs').readFileSync(`${__dirname}/data/default.sql`, 'utf8'), (err) => {
+      if (err) return res.send({status: `error: ${err}`});
+      res.send({status: 'success'});
+    });
+  } else {
+    res.send({datasets: ['default']});
+  }
+});
+
 router.use('/', (req, res) => {
   res.send({
     paths: [
       {'/kill': 'Terminates the service (used for CI routine)'},
       {'/coverage': 'View the current code coverage report'},
-      {'/coverage/download': 'Download the code coverage report'}
+      {'/coverage/download': 'Download the code coverage report'},
+      {'/data': 'Initialize datasets'}
     ]
   });
 });
